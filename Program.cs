c5286ece434e27b85c9fffc5856bd1576cf7bb58
aro_programer <aro.programer@gmail.com>
2021-03-05 14:33:52 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Array_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 8, 3, 4, 2, 8, 7, 6, 1, 4, 1};
+            int[] _array = ConectTwoElements(ref array);
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.WriteLine(_array[i]);
+            }
+        }
+        public static int[] ConectTwoElements(ref int[] array)
+        {
+            int[] _array = new int[10];
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                _array[i] = array[i];
+            }
+            int j = 0;
+            for (int i = array.Length - 1; i >= array.Length / 2; i--)
+            {
+                _array[j++] += array[i];
+            }
+            return array;
+        }
+    }
+}
